336dd0eb7828ee918384e3d50434fc21030088d3
AbhirupChakraborty <abhirup.chakraborty1998@gmail.com>
2023-08-29 22:33:16 +0530

    MongoDB database Connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express()
 app.listen(2000)
-
-
 const userRouter = express.Router()
 app.use('/enquiry', userRouter);
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/enquirydata');
+console.log('Database Connected');
+
+const userscm = new mongoose.Schema({});
+const collect = mongoose.model('enquiry', userscm, 'enquiry');
+
+var dat=collect.find({},{ _id:0 }).then(function(data){
+    console.log(data);
+});
